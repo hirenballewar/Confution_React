2364ae0c10363e79ef344a5dbdb2f47c2181e0fd
Hiren Ballewar <hirenballewar@yahoo.com>
2021-02-28 11:37:19 +0000

    React Animation

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -21,7 +21,8 @@ function RenderCard({item, isLoading, errMess}) {
     else 
         return(
             <Card>
-                <CardImg src={baseUrl + item.image} alt={item.name} />                <CardBody>
+                <CardImg src={baseUrl + item.image} alt={item.name} />
+                 <CardBody>
                 <CardTitle>{item.name}</CardTitle>
                 {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null }
                 <CardText>{item.description}</CardText>
--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -48,27 +48,26 @@ export const postComment = (dishId, rating, author, comment) => (dispatch) => {
 
 export const fetchDishes = () => (dispatch) => {
 
-    dispatch(dishesLoading(true));
+  dispatch(dishesLoading(true));
 
-    return fetch(baseUrl + 'dishes')
-    .then(response => {
-        if (response.ok) {
-          return response;
-        } else {
-          var error = new Error('Error ' + response.status + ': ' + response.statusText);
-          error.response = response;
-          throw error;
-        }
-      },
-      error => {
-            var errmess = new Error(error.message);
-            throw errmess;
-      })
-    .then(response => response.json())
-    .then(dishes => dispatch(addDishes(dishes)))
-    .catch(error => dispatch(dishesFailed(error.message)));
+  return fetch(baseUrl + 'dishes')
+  .then(response => {
+      if (response.ok) {
+        return response;
+      } else {
+        var error = new Error('Error ' + response.status + ': ' + response.statusText);
+        error.response = response;
+        throw error;
+      }
+    },
+    error => {
+          var errmess = new Error(error.message);
+          throw errmess;
+    })
+  .then(response => response.json())
+  .then(dishes => dispatch(addDishes(dishes)))
+  .catch(error => dispatch(dishesFailed(error.message)));
 }
-
 
 export const dishesLoading = () => ({
     type: ActionTypes.DISHES_LOADING
